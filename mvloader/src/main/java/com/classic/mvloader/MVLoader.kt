484062e85b9d3b4577db5adddf0afc29f8dbd524
amjad masri <a.masri68@gmail.com>
2019-10-12 11:33:06 +0300

    refactoring (MVLoader) : move cache size attribute from MVloader  to the CacheManager and add some documentation comments

--- mvloader/src/main/java/com/classic/mvloader/MVLoader.kt
+++ mvloader/src/main/java/com/classic/mvloader/MVLoader.kt
@@ -9,28 +9,45 @@ import androidx.annotation.RequiresApi
 
 class MVLoader private constructor(context: Context) {
     companion object : SingletonHolder<MVLoader, Context>(::MVLoader)
+    private var context:Context
+    private var cacheManager:CacheManager
+
+    /**
+     * default constructor used for singleton instance
+     * sets default values for managers
+     */
+    init {
+        this.context=context.applicationContext
+        this.cacheManager=LRUCacheManager()
+    }
+
+    /**
+     * secondary constructor used by the builder to create instance with custom values
+     */
     constructor(context: Context,cacheSize: Int) : this(context) {
-        this.cacheSize=cacheSize
+        this.cacheManager=LRUCacheManager(cacheSize)
         this.context=context
     }
 
-    private var context:Context
-    private var cacheSize:Int
-
-    init {
-        this.context=context.applicationContext
-        cacheSize=10
-
-
-    }
-
+    /**
+     * set the cache size for the cache manager
+     * requires LOLLIPOP
+     * if client is less than LOLLIPOP then use the builder to set the cache size when creating the instance
+     * but keep in mind that the instance returned by the builder is not a singleton and each call to the builder
+     * will return a new instance that the client code should handle
+     */
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     fun setCasheSize(cacheSize:Int) :MVLoader{
-        this.cacheSize=cacheSize
+        cacheManager.resize(cacheSize)
         return this
     }
 
 
+    /**
+     * builder class for creating an instance of the MVLoader with custom configurations
+     * each call to the builder will create a new instance of the MVLoader and the client should handle
+     * keeping the instances singleton if needed
+     */
     data class Builder(private var context :Context
     ,private var cacheSize: Int?=null){
         fun setCacheSize(cacheSize: Int)=apply{this.cacheSize=cacheSize}
