b3b7ab976383e86c2d382ad8342041a2feb5daca
amjad masri <a.masri68@gmail.com>
2019-10-14 11:29:18 +0300

    feature(viewmodel): viewmodels

--- app/src/main/java/demo/mindvalleytest/viewModels/ImagesListViewModel.kt
+++ app/src/main/java/demo/mindvalleytest/viewModels/ImagesListViewModel.kt
@@ -1,7 +1,22 @@
-package demo.mindvalleytest.ViewModels
+package demo.mindvalleytest.viewModels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.paging.PagedList
+import demo.mindvalleytest.data.models.local.MvImagesLocal
+import demo.mindvalleytest.data.repositories.MVImageRepository
+import demo.mindvalleytest.utilities.Resource
+import javax.inject.Inject
 
-class ImagesListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class ImagesListViewModel @Inject constructor( private val mvImageRepository: MVImageRepository) : ViewModel() {
+
+    private var pagedMvImagesLocalList: LiveData<Resource<PagedList<MvImagesLocal>>> = MutableLiveData<Resource<PagedList<MvImagesLocal>>>()
+
+    fun getPagedMvImagesLocalList():LiveData<Resource<PagedList<MvImagesLocal>>>{
+        pagedMvImagesLocalList=mvImageRepository.getPagedRemotePopularMovieList(1)
+
+        return pagedMvImagesLocalList
+    }
+
 }
