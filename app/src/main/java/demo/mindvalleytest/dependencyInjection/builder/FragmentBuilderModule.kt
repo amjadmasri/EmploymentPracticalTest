f648706b1840a63ae01f0e0c1fb06c8347e68964
amjad masri <a.masri68@gmail.com>
2019-10-14 11:18:48 +0300

    demo application (image list fragment) : add fragment for image list

--- app/src/main/java/demo/mindvalleytest/dependencyInjection/builder/FragmentBuilderModule.kt
+++ app/src/main/java/demo/mindvalleytest/dependencyInjection/builder/FragmentBuilderModule.kt
@@ -2,11 +2,12 @@ package demo.mindvalleytest.dependencyInjection.builder
 
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
+import demo.mindvalleytest.dependencyInjection.modules.MVImageAdapterModule
 import demo.mindvalleytest.ui.ImagesListFragment
 
 @Module
 abstract class FragmentBuilderModule {
 
-    @ContributesAndroidInjector
+    @ContributesAndroidInjector(modules = [MVImageAdapterModule::class])
     abstract fun contributeImagesListFragment(): ImagesListFragment
 }
--- app/src/main/java/demo/mindvalleytest/ui/ImagesListFragment.kt
+++ app/src/main/java/demo/mindvalleytest/ui/ImagesListFragment.kt
@@ -3,11 +3,28 @@ package demo.mindvalleytest.ui
 import android.content.Context
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import demo.mindvalleytest.ViewModels.ImagesListViewModel
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.paging.PagedList
+import androidx.recyclerview.widget.GridLayoutManager
+import demo.mindvalleytest.viewModels.ImagesListViewModel
 import demo.mindvalleytest.R
+import demo.mindvalleytest.viewModels.ViewModelProviderFactory
+import demo.mindvalleytest.data.models.local.MvImagesLocal
+import demo.mindvalleytest.ui.adapters.MVImagePagedAdapter
+import demo.mindvalleytest.utilities.Resource
+import demo.mindvalleytest.utilities.Status
+import kotlinx.android.synthetic.main.fragment_images_list.*
+import javax.inject.Inject
+import javax.inject.Provider
 
 
 class ImagesListFragment : BaseFragment() {
+
+    @Inject
+    lateinit var viewModelProviderFactory: ViewModelProviderFactory
     override fun getLayoutRes(): Int = R.layout.fragment_images_list
 
 
@@ -21,11 +38,36 @@ class ImagesListFragment : BaseFragment() {
 
     private lateinit var viewModel: ImagesListViewModel
 
+    @Inject
+    lateinit var gridLayoutManager: Provider<GridLayoutManager>
+
+    @Inject
+    lateinit var mvImagePagedAdapter: MVImagePagedAdapter
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(ImagesListViewModel::class.java)
+
+
 
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProviders.of(this,viewModelProviderFactory).get(ImagesListViewModel::class.java)
+
+        image_recycler_view.layoutManager=gridLayoutManager.get()
+        image_recycler_view.adapter=mvImagePagedAdapter
+
+        val imagesObserver = Observer<Resource<PagedList<MvImagesLocal>>> { data ->
+            Log.d("amjad","in observing "+data.status)
+            Log.d("amjad","in observing "+ (data.data?.size ?: -1))
+            if(data.status == Status.SUCCESS)
+                mvImagePagedAdapter.submitList(data.data)
+            else if (data.status.equals(Status.ERROR))
+                Toast.makeText(activity,"there was an error",Toast.LENGTH_LONG).show()
+        }
+
+        viewModel.getPagedMvImagesLocalList().observe(this,imagesObserver)
+    }
 }
